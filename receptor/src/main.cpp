63bd417f7e3bd7ff2da057ee14642ebf4e0d1618
Ekers <105935934+ekeers@users.noreply.github.com>
2023-10-27 18:52:48 +0200

    5 bits asincronos

--- receptor/src/main.cpp
+++ receptor/src/main.cpp
@@ -4,14 +4,14 @@
 #define dataPin 13
 
 int sincTime = 10;
-int pulseTime = 100;
-int data[6];
+int pulseTime = 1000;
+int data[5];
 int total;
 
 int x = 0;
 int y = 0;
 
-char index[37] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'};
+char index[28] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 //generamos un indice comun para que los dos programas tengan un codigo comun
 
 const int rs = 9, en = 8, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
@@ -27,13 +27,13 @@ void loop() {
   while(digitalRead(dataPin) == LOW){}
   while(digitalRead(dataPin) == HIGH){}
   delay(sincTime);
-  for(int i = 5; i > -1;i--){
-    delay(pulseTime);
+  for(int i = 4; i > -1;i--){
     data[i] = digitalRead(dataPin);
     Serial.print(data[i]);
+    delay(pulseTime);
   }
 
-  total =data[5]*32 + data[4]*16 + data[3]*8 + data[2]*4 + data[1]*2 + data[0]*1;
+  total = data[4]*16 + data[3]*8 + data[2]*4 + data[1]*2 + data[0]*1;
   Serial.print(index[total]);
   Serial.println();
  
